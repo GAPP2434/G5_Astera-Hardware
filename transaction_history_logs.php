b6ac7cdec90e0cac3f8b85406867114872f2b4e6
kyrzzz <150363053+kyrzzz@users.noreply.github.com>
2024-04-25 22:28:18 +0800

    Added DataTable to Transaction History
    DataTable. Allows Searching. Needs to test with user-cases e.g, >100 entries.
    Page itself lacks design and uses placehlder text
    
    Todo:
    Export Button
    Refresh Table Button

--- transaction_history_logs.php
+++ transaction_history_logs.php
@@ -1,7 +1,12 @@
+<?php 
+session_start();
+$username = $_SESSION['username']; 
+if(!isset($username)){
+    header("Location: login.html");
+}
+?>
 <!doctype html>
 <html lang="en">
-
- 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -12,6 +17,8 @@
     <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/libs/css/style.css">
     <link rel="stylesheet" href="assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
 </head>
 
 <body>
@@ -63,7 +70,47 @@
                 <!-- ============================================================== -->
                 <div class="row">
                     <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <h3 class="text-center">Content goes here!</h3>
+                        <div class="card">
+                            <h5 class="card-header">DataTable Example</h5>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="example" class="table table-striped table-bordered">
+                                        <thead>
+                                            <tr>
+                                                <th>Item Code</th>
+                                                <th>Action</th>
+                                                <th>Quantity In</th>
+                                                <th>Quantity Out</th>
+                                                <th>Total</th>
+                                                <th>Date Modified</th>
+                                                <th>User</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php 
+                                                // Include the database connection file
+                                                include 'dbcon.php';
+
+                                                // Query to fetch data from the table
+                                                $query = "SELECT dItemCode, dType, dQty_in, dQty_out, dQty_total, dDateAdded, dUsername FROM tblitemhistory";
+                                                $result = mysqli_query($conn, $query);
+                                                while($row = mysqli_fetch_assoc($result)) {
+                                            ?>
+                                            <tr>
+                                                <td><?php echo $row['dItemCode']; ?></td>
+                                                <td><?php echo $row['dType']; ?></td>
+                                                <td><?php echo $row['dQty_in']; ?></td>
+                                                <td><?php echo $row['dQty_out']; ?></td>
+                                                <td><?php echo $row['dQty_total']; ?></td>
+                                                <td><?php echo $row['dDateAdded']; ?></td>
+                                                <td><?php echo $row['dUsername']; ?></td>
+                                            </tr>
+                                            <?php } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -102,6 +149,17 @@
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
     <script src="assets/vendor/slimscroll/jquery.slimscroll.js"></script>
     <script src="assets/libs/js/main-js.js"></script>
+    <!-- DataTables JS -->
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap4.min.js"></script>
+    <!-- DataTables Initialization -->
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+                searching: true
+            });
+        });
+    </script>
 </body>
  
 </html>
